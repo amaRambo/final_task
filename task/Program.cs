ba9e4603fb072718052b2a5f1e65fd8de52b4c06
Ilya Tsai <ilya_1_3_5@mail.ru>
2022-11-24 19:40:55 +0500

    task ready

--- task/Program.cs
+++ task/Program.cs
@@ -1,3 +1,11 @@
 ﻿
 
 
+string[] array = {"asdasd", "asdas", "asdasd", "asd", "q", "123"};
+for (int i = 0; i < array.Length; i++)
+{
+    if(array[i].Length <= 3)
+    {
+        Console.WriteLine(array[i]); 
+    }
+}
